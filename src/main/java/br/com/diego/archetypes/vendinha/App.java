f8ed4306b4d626386091974e910dbc83a74a60d0
TRIERSISTEMAS\jovem.dsv <teste>
2021-07-29 11:50:07 -0300

    Update project

--- src/main/java/br/com/diego/archetypes/vendinha/App.java
+++ src/main/java/br/com/diego/archetypes/vendinha/App.java
@@ -12,24 +12,28 @@ public class App {
     public static void main(String[] args) {
         
         Produto p1 = new Produto("cenoura", BigDecimal.valueOf(0.5));
-        Produto p2 = new Produto("beterraba", BigDecimal.valueOf(0.6));
+        //Produto p2 = new Produto("beterraba", BigDecimal.valueOf(0.6));
        
         Cliente c = new Cliente("Carlos");
         
         Compra compra = c.comprar(p1, 5);
         
-        compra.addProduto(p2, 2);
+        //compra.addProduto(p2, 2);
         
               
         System.out.println("Cliente....: " + compra.getCliente().getId() + " - " + compra.getCliente().getNome());
+        
+        System.out.println("\nStatus.....: " + compra.getStatusPagamento());
+        System.out.println("Comprado...: " + compra.getDataCompra());
     	for (ItemCompra i : compra.getItens()) {
-			System.out.println("");
+    		System.out.println("");
     		System.out.println("Produto....: " + i.getProduto().getNome());
     		System.out.println("Qtd........: " + i.getQtd());
     		System.out.println("Valor UN...: " + i.getProduto().getValor());
     		System.out.println("Total......: " + i.getTotal());
 		}
     	System.out.println("\nTotal G....: " + compra.getTotal());
-        
+    	System.out.println("\nTroco......: " + compra.pagar(BigDecimal.valueOf(100,00)));    	   	
+    	
     }
 }
--- src/main/java/br/com/diego/vendinha/domain/Compra.java
+++ src/main/java/br/com/diego/vendinha/domain/Compra.java
@@ -2,24 +2,87 @@ package br.com.diego.vendinha.domain;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.SplittableRandom;
 
 public class Compra {	
 	
+	private EnumStatusPagamento statusPagamento;
 	private Long id;	
+	private BigDecimal valorPago;
 	private Cliente cliente;
 	private List<ItemCompra> itens;
+	private LocalDateTime dataCompra, dataPagamento, dataCancelamento;
 	
 	public Compra(Cliente c, Produto p, Integer qtd) {
 		this.itens = new ArrayList<>();
 		this.id = new SplittableRandom().nextLong(1, Long.MAX_VALUE);
 		this.cliente = c;
+		this.dataCompra = LocalDateTime.now();
+		this.statusPagamento = EnumStatusPagamento.AGUARDANDO;
 		this.addProduto(p, qtd);
 	}
 
 	public void addProduto(Produto p, Integer qtd) {
 		this.itens.add(new ItemCompra(p, qtd));
+	}
+	
+	public BigDecimal pagar(BigDecimal valor) {
+		
+		if(statusPagamento.equals(EnumStatusPagamento.PAGO)) {
+			throw new RuntimeException("Pagamento já realizado!");
+		}
+		
+		if(Objects.isNull(valor)) {
+			throw new RuntimeException("Dinheiro inválido!");
+		}
+		
+		if(valor.compareTo(this.getTotal()) < 0) {
+			throw new RuntimeException("Dinheiro insuficiente!");
+		}
+		
+		if(statusPagamento.equals(EnumStatusPagamento.CANCELADO)) {
+			this.dataCancelamento = null;
+		}
+		
+		this.statusPagamento = EnumStatusPagamento.PAGO;
+		this.dataPagamento = LocalDateTime.now();
+		this.valorPago = valor;
+		
+		return getTotal().subtract(valor);
+		
+		
+		//Validar o valor
+	}
+	
+	public void cancelaPagamento() { //Cancela
+		this.statusPagamento = EnumStatusPagamento.CANCELADO;
+		this.dataPagamento = LocalDateTime.now();
+		
+		if(this.statusPagamento == EnumStatusPagamento.PAGO) {
+			getTotal().add(valorPago);
+		}
+		//Calcular estorno - ok
+	}
+	
+	public EnumStatusPagamento getStatusPagamento() {
+		return statusPagamento;
+	}
+	
+	public LocalDateTime getDataCompra() {
+		return dataCompra;
+	}
+	
+	public LocalDateTime getDataPagamento() {
+		dataPagamento = LocalDateTime.now();
+		return dataPagamento;
+	}
+	
+	public LocalDateTime getDataCancelamento() {
+		dataCancelamento = LocalDateTime.now();
+		return dataCancelamento;
 	}
 	
 	public Cliente getCliente() {
